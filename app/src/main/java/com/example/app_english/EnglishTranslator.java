627034960daa5c085d67316b3a035731d07d69ba
Admin <thithutrang4869@gmail.com>
2024-01-14 16:49:58 +0700

    App english

---
+++ app/src/main/java/com/example/app_english/EnglishTranslator.java
@@ -0,0 +1,14 @@
+package com.example.app_english;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EnglishTranslator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_english_translator);
+    }
+}
---
+++ app/src/main/java/com/example/app_english/myVocabulary.java
@@ -0,0 +1,14 @@
+package com.example.app_english;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class myVocabulary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_vocabulary);
+    }
+}
