d160c72d977783eee132877b20843aac364b3963
TrungThiLinh <91339338+TrungThiLinh@users.noreply.github.com>
2024-01-16 12:04:29 +0700

    app dev

---
+++ app/src/main/java/com/example/app_english/myVocabulary.java
@@ -0,0 +1,30 @@
+package com.example.app_english;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class myVocabulary extends AppCompatActivity {
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_vocabulary);
+
+        fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(myVocabulary.this, UploadActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
