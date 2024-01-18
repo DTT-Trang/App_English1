6a6930f17900127299fe1dcab63f57b1efbb9958
Admin <thithutrang4869@gmail.com>
2024-01-18 23:32:20 +0700

    App english

--- app/src/main/java/com/example/app_english/Food.java
+++ app/src/main/java/com/example/app_english/Food.java
@@ -2,15 +2,27 @@ package com.example.app_english;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class Food extends AppCompatActivity {
 
-
+    Button btnQuiz;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
+
+        btnQuiz = findViewById(R.id.btnQuiz);
+
+        btnQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Food.this,Quiz.class);
+                startActivity(intent);
+            }
+        });
     }
 }
